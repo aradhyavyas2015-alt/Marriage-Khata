6e6e34b9191806b2446593608cf878b20f174971
aradhyavyas2015-alt <aradhyavyas2015@gmail.com>
2026-07-15 01:56:18 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'shaadi-khata-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE).then(function(cache){ return cache.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request).then(function(res){
+        return caches.open(CACHE).then(function(cache){
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      }).catch(function(){
+        return cached;
+      });
+    })
+  );
+});
